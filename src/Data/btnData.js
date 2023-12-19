08ab96fd3a4abc415ba55dd4b5a42a75266bd113
mayankSonkar2@14 <55187056+Mayank1-cpu@users.noreply.github.com>
2023-12-19 20:12:37 +0530

    starte files

---
+++ src/Data/btnData.js
@@ -0,0 +1,18 @@
+export const btnData = [
+  {
+    id: 1,
+    label: "Btn 1",
+  },
+  {
+    id: 2,
+    label: "Btn 2",
+  },
+  {
+    id: 3,
+    label: "Btn 3",
+  },
+  {
+    id: 4,
+    label: "Btn 4",
+  },
+];
